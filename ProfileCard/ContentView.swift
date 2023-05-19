5a4186f5cd1759c19d703bd3a8ba113f7e150db9
ChiduAnush <chiduanush1@gmail.com>
2023-05-19 14:33:00 +0530

    made the original card

--- ProfileCard/ContentView.swift
+++ ProfileCard/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+
+        VStack(alignment: .leading, spacing: 20.0) {
+            Image("ngnl-zero")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(20)
+            
+            HStack {
+                Text("No Game No Life\n:zero")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                
+                Spacer()
+                
+                VStack {
+                    HStack {
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.leadinghalf.fill")
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                    
+                    Text("(reviews 342)")
+                        .font(.caption)
+                }
+            }
+            
+            Text("A story about love, sacrifice, and belief. sequel to the anime.")
+            
+            HStack {
+                Spacer()
+                Image(systemName: "binoculars.fill").foregroundColor(.gray)
+                Image(systemName: "heart").foregroundColor(.red)
+            }
+            .font(.body)
+
         }
-        .padding()
+
+        
+        
     }
 }
 
