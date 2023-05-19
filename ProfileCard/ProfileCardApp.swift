d946bebe86dce1b082b47d19d5cfcaeef95c2350
ChiduAnush <chiduanush1@gmail.com>
2023-05-19 13:19:12 +0530

    Initial Commit

---
+++ ProfileCard/ProfileCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileCardApp.swift
+//  ProfileCard
+//
+//  Created by Chidu Anush on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ProfileCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
