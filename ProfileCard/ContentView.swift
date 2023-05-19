951bb13e36d50329fdf5403162f3e86388a737f5
ChiduAnush <chiduanush1@gmail.com>
2023-05-19 14:38:56 +0530

    added background colour

--- ProfileCard/ContentView.swift
+++ ProfileCard/ContentView.swift
@@ -9,53 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-
-        VStack(alignment: .leading, spacing: 20.0) {
-            Image("ngnl-zero")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(20)
-            
-            HStack {
-                Text("No Game No Life\n:zero")
-                    .font(.title3)
-                    .fontWeight(.semibold)
-                
-                Spacer()
-                
-                VStack {
-                    HStack {
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.leadinghalf.fill")
-                    }
-                    .foregroundColor(.orange)
-                    .font(.caption)
-                    
-                    Text("(reviews 342)")
-                        .font(.caption)
-                }
-            }
-            
-            Text("A story about love, sacrifice, and belief. sequel to the anime.")
-            
-            HStack {
-                Spacer()
-                Image(systemName: "binoculars.fill").foregroundColor(.gray)
-                Image(systemName: "heart").foregroundColor(.red)
-            }
-            .font(.body)
-
-        }
-        .padding()
-        .background(Rectangle()
-            .foregroundColor(.white)
-            .cornerRadius(10)
-            .shadow(radius: 20))
-        .padding()
         
+        ZStack {
+            Color(.systemTeal)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("ngnl-zero")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(20)
+                
+                HStack {
+                    Text("No Game No Life\n:zero")
+                        .font(.title3)
+                        .fontWeight(.semibold)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.fill")
+                        }
+                        .foregroundColor(.orange)
+                        .font(.caption)
+                        
+                        Text("(reviews 342)")
+                            .font(.caption)
+                    }
+                }
+                
+                Text("A story about love, sacrifice, and belief. sequel to the anime.")
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "binoculars.fill").foregroundColor(.gray)
+                    Image(systemName: "heart").foregroundColor(.red)
+                }
+                .font(.body)
+
+            }
+            .padding()
+            .background(Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 20))
+            .padding()
+            
+        }
+
+
         
     }
 }
